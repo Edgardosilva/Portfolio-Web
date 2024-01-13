9aa98216bcf93dd1647a0001d492cb7d960bd65f
Edgardosilva <edgardosilva.es@gmail.com>
2024-01-13 17:14:23 -0300

    working on individual projects

---
+++ src/components/Slider.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Slider = ({ img1, img2, img3 }) => {
+  return (
+    <div className='rounded-xl w-full h-42 relative flex overflow-scroll snap-mandatory '>
+      <img src={img1} alt="img1Slider" className='w-full left-0 sticky object-cover snap-center' />
+      <img src={img2} alt="img2Slider" className='w-full left-0 sticky object-cover snap-center'/>
+      <img src={img3} alt="img3Slider" className='w-full left-0 sticky object-cover snap-center'/>  
+    </div>
+  );
+};
+
+export default Slider;
