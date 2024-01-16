d0bfc97eeb082c50cde1dd8094e824e54d243283
Edgardosilva <edgardosilva.es@gmail.com>
2024-01-15 22:20:04 -0300

    first bento grids desing finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,9 +9,15 @@ const Navbar = () => {
 	}
 
 	return (
-		<nav className="p-4 flex flex-col fixed z-50 w-full bg-black bg-opacity-80 backdrop-blur-lg">
-      <img src="logo.svg" className="w-[40px]" onClick={handleClick} />
-      <ul className={`bg-black bg-opacity-80 bg-blur-lg animate-fade-right animate-duration-[300ms] flex flex-col mt-14 left-0 w-40 absolute h-screen z-50 ${isOpen? 'block' : 'hidden'}`}>
+		<nav className="p-4 flex flex-col fixed z-50 w-full bg-black bg-opacity-80 backdrop-blur-lg sm:h-18 sm:pt-7">
+      <img src="logo.svg" className="w-[40px] sm:hidden" onClick={handleClick} />
+      <ul className={`bg-black bg-opacity-80 bg-blur-lg animate-fade-right animate-duration-[300ms] w-full h-10 hidden sm:flex sm:justify-center sm:items-center sm:bg-transparent`}>
+        <li className="text-white text-lg p-3 mb-3 ml-4 lg:text-[20px]"><a href="#">Home</a></li>
+        <li className="text-white text-lg p-3 mb-3 ml-4 lg:text-[20px]"><a href="#">Proyectos</a></li>
+        <li className="text-white text-lg p-3 mb-3 ml-4 lg:text-[20px]"><a href="#">Skills</a></li>
+        <li className="text-white text-lg p-3 mb-3 ml-4 lg:text-[20px]"><a href="#">Contacto</a></li>
+      </ul>
+      <ul className={`bg-black bg-opacity-80 bg-blur-lg animate-fade-right animate-duration-[300ms] flex flex-col mt-14 left-0 w-40 absolute h-screen z-50 ${isOpen? 'block' : 'hidden'} sm:hidden`}>
         <li className="text-white text-lg p-3 mb-3 ml-4"><a href="#">Home</a></li>
         <li className="text-white text-lg p-3 mb-3 ml-4"><a href="#">Proyectos</a></li>
         <li className="text-white text-lg p-3 mb-3 ml-4"><a href="#">Skills</a></li>
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Slider = ({ img1, img2, img3 }) => {
   return (
-    <div className='rounded-xl w-full h-42 relative flex overflow-scroll snap-mandatory '>
+    <div className='rounded-xl w-full h-42 relative flex overflow-scroll snap-mandatory mt-3'>
       <img src={img1} alt="img1Slider" className='w-full left-0 sticky object-cover snap-center' />
       <img src={img2} alt="img2Slider" className='w-full left-0 sticky object-cover snap-center'/>
       <img src={img3} alt="img3Slider" className='w-full left-0 sticky object-cover snap-center'/>  
