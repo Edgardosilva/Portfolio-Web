611fa4e2e6c3119316b021e6759f00d350a71a77
Edgardosilva <edgardosilva.es@gmail.com>
2024-01-16 19:21:12 -0300

    web bento grids finished

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Slider = ({ img1, img2, img3 }) => {
   return (
-    <div className='rounded-xl w-full h-42 relative flex overflow-scroll snap-mandatory mt-3'>
-      <img src={img1} alt="img1Slider" className='w-full left-0 sticky object-cover snap-center' />
-      <img src={img2} alt="img2Slider" className='w-full left-0 sticky object-cover snap-center'/>
-      <img src={img3} alt="img3Slider" className='w-full left-0 sticky object-cover snap-center'/>  
+    <div className='rounded-xl w-[350px] h-[234px] relative flex overflow-scroll snap-mandatory mt-3 lg:mt-0 lg:overflow-visible lg:shadow-xl'>
+      <img src={img1} alt="img1Slider" className='w-full left-0 sticky object-cover snap-center rounded-xl border border-slate-700' />
+      <img src={img2} alt="img2Slider" className='w-full left-0 sticky object-cover snap-center lg:hidden'/>
+      <img src={img3} alt="img3Slider" className='w-full left-0 sticky object-cover snap-center lg:hidden'/>  
     </div>
   );
 };
