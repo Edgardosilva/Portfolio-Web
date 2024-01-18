51047b7a490bb04d3dc8102ba83476223ac23426
Edgardosilva <edgardosilva.es@gmail.com>
2024-01-17 22:00:28 -0300

    sm bento grids avance

---
+++ src/components/CategoryNav.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const CategoryNav = ({ text }) => {
+    return (
+ 
+        <a href="#" class="group relative inline-block overflow-hidden rounded  bg-transparent  px-12 py-3 text-lg font-medium text-slate-300 hover:text-cyan-500 focus:outline-none">
+            <span class="ease absolute left-0 top-0 h-0 w-0 border-t-2 border-cyan-600 transition-all duration-200 group-hover:w-full"></span>
+            <span class="ease absolute right-0 top-0 h-0 w-0 border-r-2 border-cyan-600 transition-all duration-200 group-hover:h-full"></span>
+            <span class="ease absolute bottom-0 right-0 h-0 w-0 border-b-2 border-cyan-600 transition-all duration-200 group-hover:w-full"></span>
+            <span class="ease absolute bottom-0 left-0 h-0 w-0 border-l-2 border-cyan-600 transition-all duration-200 group-hover:h-full"></span>
+            <p className='w-24 text-center'>{text}</p>
+        </a>
+      
+    );
+};
+
+export default CategoryNav;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
+import CategoryNav from './CategoryNav.jsx';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,18 +11,18 @@ const Navbar = () => {
 
 	return (
 		<nav className="p-4 flex flex-col fixed z-50 w-full bg-black bg-opacity-80 backdrop-blur-lg sm:h-18 sm:pt-7">
-      <img src="logo.svg" className="w-[40px] sm:hidden" onClick={handleClick} />
-      <ul className={`bg-black bg-opacity-80 bg-blur-lg animate-fade-right animate-duration-[300ms] w-full h-10 hidden sm:flex sm:justify-center sm:items-center sm:bg-transparent`}>
-        <li className="text-white text-lg p-3 mb-3 ml-4 lg:text-[20px]"><a href="#">Home</a></li>
-        <li className="text-white text-lg p-3 mb-3 ml-4 lg:text-[20px]"><a href="#">Proyectos</a></li>
-        <li className="text-white text-lg p-3 mb-3 ml-4 lg:text-[20px]"><a href="#">Skills</a></li>
-        <li className="text-white text-lg p-3 mb-3 ml-4 lg:text-[20px]"><a href="#">Contacto</a></li>
+      <img src="logo.svg" className="w-[40px] md:hidden" onClick={handleClick} />
+      <ul className={`bg-black bg-opacity-80 bg-blur-lg animate-fade-right animate-duration-[300ms] w-full h-10 hidden md:flex sm:justify-center md:items-center md:bg-transparent`}>
+        <li><CategoryNav text = 'Home'/></li>
+        <li><CategoryNav text = 'Sobre mi'/></li>
+        <li><CategoryNav text = 'Proyetos'/></li>
+        <li><CategoryNav text = 'Contacto'/></li>
       </ul>
-      <ul className={`bg-black bg-opacity-80 bg-blur-lg animate-fade-right animate-duration-[300ms] flex flex-col mt-14 left-0 w-40 absolute h-screen z-50 ${isOpen? 'block' : 'hidden'} sm:hidden`}>
-        <li className="text-white text-lg p-3 mb-3 ml-4"><a href="#">Home</a></li>
-        <li className="text-white text-lg p-3 mb-3 ml-4"><a href="#">Proyectos</a></li>
-        <li className="text-white text-lg p-3 mb-3 ml-4"><a href="#">Skills</a></li>
-        <li className="text-white text-lg p-3 mb-3 ml-4"><a href="#">Contacto</a></li>
+      <ul className={` bg-black bg-opacity-80 bg-blur-lg animate-fade-right animate-duration-[300ms] flex flex-col mt-20 left-0 w-42 absolute h-screen z-50 ${isOpen? 'block' : 'hidden'} md:hidden`}>
+        <li className='mb-5'><CategoryNav text = 'Home'/></li>
+        <li className='mb-5'><CategoryNav text = 'Sobre mi'/></li>
+        <li className='mb-5'><CategoryNav text = 'Proyetos'/></li>
+        <li className='mb-5'><CategoryNav text = 'Contacto'/></li>
       </ul>
 		</nav>
   );
