d397887985d98118895d9c0f8b912839229f9d29
Edgardosilva <edgardosilva.es@gmail.com>
2024-01-18 21:10:59 -0300

    sm-bento avance 2

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Slider = ({ img1, img2, img3 }) => {
   return (
-    <div className='rounded-xl w-[350px] h-[234px] relative flex overflow-scroll snap-mandatory mt-3 lg:mt-0 lg:overflow-visible lg:shadow-xl'>
+    <div className='rounded-xl relative flex overflow-scroll snap-mandatory mt-3 lg:mt-0 lg:overflow-visible lg:shadow-xl'>
       <img src={img1} alt="img1Slider" className='w-full left-0 sticky object-cover snap-center rounded-xl border border-slate-700' />
       <img src={img2} alt="img2Slider" className='w-full left-0 sticky object-cover snap-center lg:hidden'/>
       <img src={img3} alt="img3Slider" className='w-full left-0 sticky object-cover snap-center lg:hidden'/>  
