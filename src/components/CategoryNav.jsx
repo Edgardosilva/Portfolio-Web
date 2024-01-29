f52c3fea70f2be8826e91f9b897d2c022cd55a6c
Edgardosilva <edgardosilva.es@gmail.com>
2024-01-29 17:42:48 -0300

    navbar and download btn fixed

--- src/components/CategoryNav.jsx
+++ src/components/CategoryNav.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
-const CategoryNav = ({ text, id }) => {
+const CategoryNav = ({ text, route }) => {
     return (
  
-        <a href={id} className="group relative inline-block overflow-hidden rounded  bg-transparent  px-12 py-3 text-lg font-medium text-slate-300 hover:text-cyan-500 focus:outline-none">
+        <a href={route} className=" hover:cursor-pointer group relative inline-block overflow-hidden rounded  bg-transparent  px-12 py-3 text-lg font-medium text-slate-300 hover:text-cyan-500 focus:outline-none">
             <span className="ease absolute left-0 top-0 h-0 w-0 border-t-2 border-cyan-600 transition-all duration-200 group-hover:w-full"></span>
             <span className="ease absolute right-0 top-0 h-0 w-0 border-r-2 border-cyan-600 transition-all duration-200 group-hover:h-full"></span>
             <span className="ease absolute bottom-0 right-0 h-0 w-0 border-b-2 border-cyan-600 transition-all duration-200 group-hover:w-full"></span>
             <span className="ease absolute bottom-0 left-0 h-0 w-0 border-l-2 border-cyan-600 transition-all duration-200 group-hover:h-full"></span>
             <p className='w-24 text-center'>{text}</p>
         </a>
+        
       
     );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,18 +11,16 @@ const Navbar = () => {
 
 	return (
 		<nav className="p-4 flex flex-col fixed z-50 w-full bg-black bg-opacity-80 backdrop-blur-lg sm:h-18 sm:pt-7">
-      <img src="logo.svg" className="w-[40px] md:hidden" onClick={handleClick} />
+      <img src="logo.svg" className="w-[40px] md:hidden hover:cursor-pointer" onClick={handleClick} />
       <ul className={`bg-black bg-opacity-80 bg-blur-lg animate-fade-right animate-duration-[300ms] w-full h-10 hidden md:flex sm:justify-center md:items-center md:bg-transparent`}>
-        <li><CategoryNav text = 'Home'/></li>
-        <li><CategoryNav text = 'Sobre mi' id='sobremi'/></li>
-        <li><CategoryNav text = 'Proyetos'/></li>
-        <li><CategoryNav text = 'Contacto'/></li>
+        <li><CategoryNav text = 'Inicio' route='#inicio'/></li>
+        <li><CategoryNav text = 'Sobre mi' route = '#sobremi'/></li>
+        <li><CategoryNav text = 'Proyectos' route ='#proyectos'/></li>
       </ul>
-      <ul className={` bg-black bg-opacity-80 bg-blur-lg animate-fade-right animate-duration-[300ms] flex flex-col mt-20 left-0 w-42 absolute h-screen z-50 ${isOpen? 'block' : 'hidden'} md:hidden`}>
-        <li className='mb-5'><CategoryNav text = 'Home'/></li>
-        <li className='mb-5'><CategoryNav text = 'Sobre mi'/></li>
-        <li className='mb-5'><CategoryNav text = 'Proyetos'/></li>
-        <li className='mb-5'><CategoryNav text = 'Contacto'/></li>
+      <ul className={` bg-black bg-opacity-80 bg-blur-lg animate-fade-right animate-duration-[300ms] flex flex-col mt-16 md:mt-20 left-0 w-42 absolute h-screen z-50 ${isOpen? 'block' : 'hidden'} md:hidden`}>
+        <li className='mb-5'><CategoryNav text = 'Inicio' route='#inicio'/></li>
+        <li className='mb-5'><CategoryNav text = 'Sobre mi' route='#sobremi'/></li>
+        <li className='mb-5'><CategoryNav text = 'Proyectos' route='#proyectos'/></li>
       </ul>
 		</nav>
   );
