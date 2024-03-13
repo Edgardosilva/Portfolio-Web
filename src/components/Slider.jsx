e89274f819d3f8bf843c03bcad25bd44132e4619
Edgardosilva <edgardosilva.es@gmail.com>
2024-03-13 12:22:46 -0300

    Update image sizes in Slider component

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const Slider = ({ img1, img2, img3 }) => {
+const Slider = ({ img1 }) => {
   return (
-    <div className='rounded-xl relative flex lg:flex-col overflow-scroll snap-mandatory mt-3 lg:mt-0 lg:overflow-visible lg:shadow-xl'>
-      <img src={img1} alt="img1Slider" className=' bg- w-full left-0 sticky object-cover snap-center rounded-xl border border-slate-700' />
-      <img src={img2} alt="img2Slider" className='w-full left-0 sticky object-cover snap-center lg:hidden'/>
-      <img src={img3} alt="img3Slider" className='w-full left-0 sticky object-cover snap-center lg:hidden'/>  
+    <div className='relative lg:shadow-xl h-full rounded-xl'>
+      <img src={img1} alt="img1Slider" className='h-full rounded-xl' />
     </div>
   );
 };
